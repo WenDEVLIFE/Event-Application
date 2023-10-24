01ef13a7f76479a9a9b56b9ff72fd0d14f822534
Frouen M. Medina Jr <117834496+WenDEVLIFE@users.noreply.github.com>
2023-10-24 13:58:15 +0800

    event system
    event system

---
+++ Event_Criteria_System/src/event_criteria_system/databaselogin_verifier.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package event_criteria_system;
+
+/**
+ *
+ * @author Administrator
+ */
+public class databaselogin_verifier extends Login {
+    public static void main(String [] args){
+        
+    }
+}
